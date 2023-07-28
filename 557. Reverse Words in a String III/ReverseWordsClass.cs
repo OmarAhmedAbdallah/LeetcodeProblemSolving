0f499cb0d2b06ab91f8804ad21e89d62ef5b6d84
OmarAhmedAbdAllah <omarbadry28@gmail.com>
2023-07-29 00:28:14 +0300

    string problems

---
+++ 557. Reverse Words in a String III/ReverseWordsClass.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCodeProblems._557._Reverse_Words_in_a_String_III
+{
+    internal class ReverseWordsClass
+    {
+        public string ReverseWords(string s)
+        {
+            string[] words = s.Split(" ");
+
+            for (int i = 0; i < words.Length; i++)
+            {
+                char[] chars = words[i].ToCharArray();
+                int l = 0, h = chars.Length - 1;
+                while (l < h)
+                {
+                    (chars[l], chars[h]) = (chars[h], chars[l]);
+                    l++;
+                    h--;
+                }
+                words[i] = new string(chars);
+            }
+            return String.Join(" ", words);
+        }
+    }
+}
---
+++ 804. Unique Morse Code Words/UniqueMorseRepresentationsClass.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCodeProblems._804._Unique_Morse_Code_Words
+{
+    internal class UniqueMorseRepresentationsClass
+    {
+    }
+}
